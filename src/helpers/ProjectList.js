55733b567e9254568db2a165959d0dff117b0983
hamzaismail4 <hbalouty22@gmail.com>
2024-02-24 19:07:52 +0200

    first commit

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,80 @@
+export const ProjectList = [
+  {
+    id: 1,
+    title: 'BogaStore',
+    code: 'https://github.com/hamzabalouty/BogaStore-1',
+    demo: 'https://bogastore22.netlify.app/',
+  },
+  {
+    id: 2,
+    title: 'MEDICAL-WEBSITE',
+    code: 'https://github.com/hamzabalouty/MEDICAL-WEBSITE',
+    demo: 'https://medicalwebsite22.netlify.app/',
+  },
+  {
+    id: 3,
+    title: 'A-Shop',
+    code: 'https://github.com/hamzabalouty/shop',
+    demo: 'https://warm-tiramisu-2190da.netlify.app/',
+  },
+  {
+    id: 4,
+    title: 'Lights-Ecommerce',
+    code: 'https://github.com/hamzabalouty/Lights-Ecommerce-1-',
+    demo: 'https://lights-ecommerce22.netlify.app/',
+  },
+  {
+    id: 5,
+    title: 'Cookware',
+    code: 'https://github.com/hamzabalouty/Cookware1',
+    demo: 'https://cookware22.netlify.app/',
+  },
+  {
+    id: 6,
+    title: 'DrugStore',
+    code: 'https://github.com/hamzabalouty/DrugStore1',
+    demo: 'https://drugstore22.netlify.app/',
+  },
+  {
+    id: 7,
+    title: 'Corporate',
+    code: 'https://github.com/hamzabalouty/CORPORATE',
+    demo: 'https://corporate22.netlify.app/',
+  },
+  {
+    id: 8,
+    title: 'Himab',
+    code: 'https://github.com/hamzabalouty/HIMAB',
+    demo: 'https://himab22.netlify.app/',
+  },
+  {
+    id: 9,
+    title: 'Crud-Project',
+    code: 'https://github.com/hamzabalouty/cruds-project',
+    demo: 'https://crudsproject22.netlify.app/',
+  },
+  {
+    id: 10,
+    title: 'Drag-Drop',
+    code: 'https://github.com/hamzabalouty/Drag---Drop',
+    demo: 'https://drag-drop22.netlify.app/',
+  },
+  {
+    id: 11,
+    title: 'Apple',
+    code: 'https://github.com/hamzabalouty/landing-page--Apple-',
+    demo: 'https://applepage22.netlify.app/',
+  },
+  {
+    id: 12,
+    title: 'movie',
+    code: 'https://github.com/hamzabalouty/movie-app',
+    demo: 'https://moviewebsite22.netlify.app/',
+  },
+  {
+    id: 13,
+    title: 'tailwind',
+    code: 'https://github.com/hamzabalouty/tailwind-project',
+    demo: 'https://tailwindproject22.netlify.app/',
+  },
+];
